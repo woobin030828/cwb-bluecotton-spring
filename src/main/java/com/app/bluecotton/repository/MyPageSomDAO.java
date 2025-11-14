20c8baf5278f56249811634191eeaaf72db510e9
Sorrondor <garlemy@gmail.com>
2025-11-14 16:40:46 +0900

    1114

--- src/main/java/com/app/bluecotton/repository/MyPageSomDAO.java
+++ src/main/java/com/app/bluecotton/repository/MyPageSomDAO.java
@@ -2,6 +2,7 @@ package com.app.bluecotton.repository;
 
 import com.app.bluecotton.domain.dto.*;
 import com.app.bluecotton.domain.vo.som.SomReviewVO;
+import com.app.bluecotton.domain.vo.som.SomVO;
 import com.app.bluecotton.mapper.MyPagePostMapper;
 import com.app.bluecotton.mapper.MyPageSomMapper;
 import lombok.RequiredArgsConstructor;
@@ -14,10 +15,10 @@ import java.util.List;
 public class MyPageSomDAO {
     private final MyPageSomMapper  myPageSomMapper;
 
-//    //    마이페이지 솜 인증 추가
-//    public void insertSomCheckWithImages(MyPageSomCheckDTO myPageSomCheckDTO) {
-//        myPageSomMapper.insertSomCheckWithImages(myPageSomCheckDTO);
-//    }
+    //    마이페이지 회원 별 파티솜 솔로솜 조회
+    public List<SomVO> selectById(Long id) {
+        return myPageSomMapper.selectById(id);
+    }
     //    마이페이지 솜 리뷰 추가
     public void insertSomReview(SomReviewVO somReviewVO) {
         myPageSomMapper.insertSomReview(somReviewVO);
@@ -30,5 +31,8 @@ public class MyPageSomDAO {
     public List<MyPageSomReviewDTO> readSomReview(Long id) {
         return myPageSomMapper.readSomReview(id);
     }
-
+    //    마이페이지 랭크 호출
+    public Long readRank(Long id) {
+        return myPageSomMapper.readRank(id);
+    }
 }
