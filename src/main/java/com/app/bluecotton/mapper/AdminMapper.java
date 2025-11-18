07d4817e9b3976dd82eadaadf3422389407111d4
taehee <taeheecafe@gmail.com>
2025-11-18 10:39:20 +0900

    관리자 삭제

--- src/main/java/com/app/bluecotton/mapper/AdminMapper.java
+++
@@ -1,9 +0,0 @@
-package com.app.bluecotton.mapper;
-
-import com.app.bluecotton.domain.dto.AdminOrderDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AdminMapper {
-    public AdminOrderDTO selectAdminOrderById(Long orderId);
-}
--- src/main/java/com/app/bluecotton/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package com.app.bluecotton.service;
-
-import com.app.bluecotton.domain.dto.AdminOrderDTO;
-
-public interface AdminService {
-    public AdminOrderDTO selectAdminOrderById(Long orderId);
-}
